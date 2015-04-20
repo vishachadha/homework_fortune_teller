12df0fafbf8b68791ca18e5f3685862e2f7e0134
vishachadha <Visha@dhcp-10-101-97-121.wireless.northwestern.private>
2015-04-20 15:41:14 -0500

    hw3_fortune_teller_part1_and_part2

--- config/routes.rb
+++ config/routes.rb
@@ -9,23 +9,23 @@ Rails.application.routes.draw do
 
   # Uncomment each route below ONE AT A TIME and debug.
 
-  # get("/zodiacs/aries",       { :controller =>  zodiacs,  action =>  "ram" })
-  # get("/zodiacs/taurus",      { :controller,   "zodiacs", :action,   "bull" })
-  # get(/zodiacs/gemini,        { :controller => "zodiacs", :action => "twins" })
-  # get("/zodiacs/cancer"       { :controller => "zodiacs"  :action => "crab" })
-  # get ("/zodiacs/leo",        { :controller => "zodiac",  :action => "lion" })
-  # get(" /zodiacs/virgo",      { :controller => "Zodiacs", :action => "maiden" })
-  # get("/zodiacs/libra",       { :controller => "zodiacs", :action => "scales" })
-  # get("/zodiac/scorpio",      { :controller => "zodiacs", :action => "scorpion" })
-  # get("/zodiacs/sagittarius", { :controller => "zodiacs", :action => "archer" })
-  # get("/zodiacs/capricorn",   { :controller => "zodiacs", :action => "goat" })
-  # get("/zodiacs/aquarius",    { :controller => "zodiacs", :action => "waterbearer" })
-  # get("/zodiacs/pisces",      { :controller => "zodiacs", :action => "fish" })
+  get("/zodiacs/aries",       { :controller =>  "zodiacs",  :action =>  "ram" })
+  get("/zodiacs/taurus",      { :controller =>   "zodiacs", :action =>   "bull" })
+  get("/zodiacs/gemini",        { :controller => "zodiacs", :action => "twins" })
+  get("/zodiacs/cancer",       { :controller => "zodiacs",  :action => "crab" })
+  get("/zodiacs/leo",        { :controller => "zodiacs",  :action => "lion" })
+  get("/zodiacs/virgo",      { :controller => "zodiacs", :action => "maiden" })
+  get("/zodiacs/libra",       { :controller => "zodiacs", :action => "scales" })
+  get("/zodiacs/scorpio",      { :controller => "zodiacs", :action => "scorpion" })
+  get("/zodiacs/sagittarius", { :controller => "zodiacs", :action => "archer" })
+  get("/zodiacs/capricorn",   { :controller => "zodiacs", :action => "goat" })
+  get("/zodiacs/aquarius",    { :controller => "zodiacs", :action => "waterbearer" })
+  get("/zodiacs/pisces",      { :controller => "zodiacs", :action => "fish" })
 
 
   # PART 2: VARIABLE ROUTES
   # =======================
 
-  # get("/signs/:the_sign",         { :controller => "zodiacs", :action => "sign" })
-  # get("/creatures/:the_creature", { :controller => "zodiacs", :action => "creature" })
+  get("/signs/:the_sign",         { :controller => "zodiacs", :action => "sign" })
+  get("/creatures/:the_creature", { :controller => "zodiacs", :action => "creature" })
 end
